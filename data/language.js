147ba6ce48c4a7df26adce9e1e8763d647daa8d8
João Paulo <jpdik@hotmail.com>
2018-05-25 01:57:47 -0300

    First commit

---
+++ data/language.js
@@ -0,0 +1,189 @@
+var language = {
+    "pt-BR":{
+        "text-hi": "Olá",
+        "text-im": "Eu sou",
+        "text-my-name": "João Paulo de Melo",
+        
+        "principal-functions": ["Desenvolvedor FULL-STACK"],
+        "text-about": "Sobre",
+        "info-about": "Atualmente terminando o ensino superior em Barbacena - MG. Já desenvolvo alguns sistemas e aplicativos\
+        com base em linguagens como Ruby on Rails, cordova, Java, entre outros, Buscando trazer conhecimento e\
+        aprendizagem em tecnologias atuais do mercado. Já possuo uma base forte em HTML, CSS e Javascript. ",
+        
+        "text-info": "Informações",
+        "info":[
+            ["Nome", "João Paulo de Melo"],
+            ["Data de Nascimento", "11/09/1995"],
+            ["E-mail", "jpmdik@gmail.com"]
+        ],
+        
+        "text-skills": "Habilidades",
+        "skills": [
+            ["HTML5", 90],
+            ["CSS", 75],
+            ["Jquery/JS", 75],
+            ["Bootstrap", 60],
+            ["Angular", 45]
+        ],
+        
+        "text-more": "Mais Informações",
+        
+        "label-systems": "Sistemas e bancos de dados",
+        "systems": [
+            "Windows",
+            "Ubuntu",
+            "MySQL",
+            "Firebase"
+        ],
+        
+        "label-languages": "Línguas",
+        "languages":[
+            ["Português", 100],
+            ["Inglês", 50]
+        ],
+        
+        "label-personality": "Personalidade",
+        "personality": [      
+            "Sério",
+            "Dinâmico",
+            "Organizado",
+            "Trabalho em equipe"
+        ],
+        
+        "label-about": "Interesses",
+        "about": [
+            "Música",
+            "Jogos",
+            "Filmes",
+            "Notícias"   
+        ],
+        
+        "text-contact": "Contato",
+        "text-curriculum": "Meu CV",
+        
+        "text-pExperience": "Experiência Profissional",
+        "experience": [
+            ["Vínculo: Programa de Monitoria, Enquadramento Funcional: Monitor ou Monitoria, Carga horária: 20",
+            "(2017 - 2017)",
+            "Monitoria de atividades complementares das disciplinas do Curso de Tecnologia em Sistemas para Internet\
+             no Laboratório de Inovação e Robótica, utilizando kits Arduino e Lego."],
+        ],
+        "text-qualifications": "Certificados e Diplomas",
+        "qualifications": [
+            ["Graduação em andamento em Sistemas Para Internet.",
+            "(2015 - Data presente)",
+            "Instituto Federal do Sudeste de Minas Gerais, IF SUDESTE MG, Brasil."],
+            
+            ["Técnico em Mecânica Industrial.",
+            "(2010 - 2012)", 
+            "SENAI - Departamento Regional de Minas Gerais, SENAI/DR/MG, Brasil."]
+        ],
+        
+        "text-contact": "Contato",
+        "descript-contact": "Gostou? Envie me um e-mail.",
+        
+        "text-your-name": "Seu nome",
+        "text-your-email": "Seu e-mail",
+        "text-your-message": "Sua mensagem",
+        "text-send": "Enviar",
+        "message-sent": "Sua mensagem foi enviada, obrigado!",
+        
+        "label-send-me": "Envie-me um e-mail",
+        "my-email": "jpmdik@gmail.com"
+        
+    },
+    "en":{
+        "text-hi": "Hi",
+        "text-im": "I'm",
+        "text-my-name": "João Paulo de Melo",
+        
+        "principal-functions": ["FULL-STACK DEVELOPER"],
+        "text-about": "About",
+        "info-about": "Currently finishing higher education in Barbacena - MG. I already develop some systems and applications\
+        based on languages ​​such as Ruby on Rails, Cordova, Java, among others, seeking to bring knowledge and\
+        learning in current market technologies. I already have a strong base in HTML, CSS and Javascript.",
+        
+        "text-info": "Info",
+        "info":[
+            ["Name", "João Paulo de Melo"],
+            ["Birthdate", "11/09/1995"],
+            ["Email", "jpmdik@gmail.com"]
+        ],
+        
+        "text-skills": "Skills",
+        "skills": [
+            ["HTML5", 90],
+            ["CSS", 75],
+            ["Jquery/JS", 75],
+            ["Bootstrap", 60],
+            ["Angular", 45]
+        ],
+        
+        "text-more": "More",
+        
+        "label-systems": "Systems and databases",
+        "systems": [
+            "Windows",
+            "Ubuntu",
+            "MySQL",
+            "Firebase"
+        ],
+        
+        "label-languages": "Languages",
+        "languages":[
+            ["Portuguese", 100],
+            ["English", 50]
+        ],
+        
+        "label-personality": "Personality",
+        "personality": [
+            "serious", 
+            "dynamic",
+            "organized",
+            "team work"
+        ],
+        
+        "label-about": "About Me",
+        "about": [
+            "Music",
+            "Games",
+            "movies",
+            "News"
+            
+        ],
+        
+        "text-contact": "Contact",
+        "text-curriculum": "My CV",
+        
+        "text-pExperience": "Profissional Experience",
+        "experience": [
+            ["Type of contract: Monitor's Program, Time Working: 20h/week",
+            "(2017 - 2017)",
+            "Monitoring of complementary activities of the subjects of the Technology Course\
+             in Internet Systems in the Laboratory of Innovation and Robotics, using Arduino and Lego kits"],
+        ],
+        "text-qualifications": "Training and Qualifications",
+        "qualifications": [
+            ["Graduation in progress in Internet Systems.",
+            "(2015 - Actual Date)",
+            "Federal Institute of the Southeast of Minas Gerais, IF SUDESTE MG, Brasil."],
+            
+            ["Industrial Mechanical Technician.",
+            "(2010 - 2012)", 
+            "SENAI - Regional Department of Minas Gerais, SENAI/DR/MG, Brasil."]
+        ],
+        
+        "text-contact": "Contact",
+        "descript-contact": "Liked? Contact me.",
+        
+        "text-your-name": "Your name",
+        "text-your-email": "Your email",
+        "text-your-message": "your message",
+        "text-send": "Send",
+        "message-sent": "Your message was sent, thank you!",
+        
+        "label-send-me": "Send me a email",
+        "my-email": "jpmdik@gmail.com"
+        
+    }
+};
