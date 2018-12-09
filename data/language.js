a5f3b43aa327e80a09f01868e0abbf554a535d81
Joao Paulo <jpmdik@gmail.com>
2018-12-09 00:27:55 -0200

    [MOD] Atualização Curriculo
    [MOD] Atualização Curriculo

--- data/language.js
+++ data/language.js
@@ -6,9 +6,7 @@ var language = {
         
         "principal-functions": ["Desenvolvedor FULL-STACK"],
         "text-about": "Sobre",
-        "info-about": "Atualmente terminando o ensino superior em Barbacena - MG. Já desenvolvo alguns sistemas e aplicativos\
-        com base em linguagens como Ruby on Rails, cordova, Java, entre outros, Buscando trazer conhecimento e\
-        aprendizagem em tecnologias atuais do mercado. Já possuo uma base forte em HTML, CSS e Javascript. ",
+        "info-about": "Tenho contribuído para minha instituição constantemente com diversos projetos e pesquisas e também auxilio os alunos dentro da instituição. Fui monitor de atividades complementares no mais recente laboratório de robótica e inovação tecnológica elaborando protótipos que utilizam Arduino e Lego. Já desenvolvo aplicativos, sistemas e serviços para diversas pessoas sendo autônomo e tenho amplo conhecimento em diferentes tecnologias Web. Atualmente, estou aprendendo Machine Learning com o objetivo de me tornar um cientista de dados.",
         
         "text-info": "Informações",
         "info":[
@@ -17,13 +15,13 @@ var language = {
             ["E-mail", "jpmdik@gmail.com"]
         ],
         
-        "text-skills": "Habilidades",
+        "text-skills": "Principais Habilidades",
         "skills": [
-            ["HTML5", 90],
-            ["CSS", 75],
-            ["Jquery/JS", 75],
-            ["Bootstrap", 60],
-            ["Angular", 45]
+            ["Web", 90],
+            ["Python", 80],
+            ["Ruby on Rails", 80],
+            ["Machine Learning", 60],
+            ["Mobile", 60]
         ],
         
         "text-more": "Mais Informações",
@@ -33,7 +31,8 @@ var language = {
             "Windows",
             "Ubuntu",
             "MySQL",
-            "Firebase"
+            "Firebase",
+            "MongoDB"
         ],
         
         "label-languages": "Línguas",
@@ -68,15 +67,82 @@ var language = {
             "Monitoria de atividades complementares das disciplinas do Curso de Tecnologia em Sistemas para Internet\
              no Laboratório de Inovação e Robótica, utilizando kits Arduino e Lego."],
         ],
+        "text-pAwards": "Minhas Premiações",
+        "awards": [
+            [
+                "gold",
+                "1º Lugar no Hackathon BNDES Negócios - Campus Party MG",
+                "(11/2018 - BNDES)",
+                "Tema: Como alavancar os negócios do BNDES através das redes sociais?",
+                "Nossa equipe desenvolveu o Chatbot Bob (Bot de operações bancárias) que trouxesse as ferramentas da BNDES de forma interativa ao usuário, que, além de trazer a comodidade, trazia facilidade ao usuário."
+            ],
+            [
+                "bronze",
+                "3º Lugar no Hackathon BNDES Transparente - Campus Party MG",
+                "(11/2018 - BNDES)",
+                "Tema: Identificar e responder fake news relacionadas ao BNDES.",
+                "Criamos um sistema através do uso de machine learning, que detectava se as notícias que fossem informadas eram Fake ou não através de um cruzamento de dados junto com a análise de contextos, no qual eram atribuídos pesos, e o sistema nos dava um veredito sobre a notícia. O sistema também poderia ir aprendendo com o tempo, desde que isso fosse feito pelos responsáveis do sistema."
+            ],
+            [
+                "gold",
+                "1º lugar na competição nacional de inteligência artificial Brazilian Knowledge Discovery in Databases 2018, promovida pela 7th Brazilian Conference on Intelligent Systems (BRACIS).",
+                "(10/2018 - BRACIS)",
+                "Tema: Previsão da produção de óleo de palma",
+                "Solução campeã da competição de aprendizagem de máquina Kaggle KDD BR 2018: https://github.com/rafjaa/KDD-BR-2018/blob/master/solution.ipynb"
+            ],
+            [
+                "silver",
+                "2º lugar no I Desafio de Startups do IFSudesteMG, campus Juiz de Fora",
+                "(08/2018 - IFSudesteMG, campus Juiz de Fora)",
+                "Tema: Desafio de Startups.",
+                "Nossa equipe apresentou uma ideia, dado nome de 'DevMirin', que seria uma plataforma de ensino remoto que influenciará jovens a desenvolverem a lógica de programação, o que será promissor dada a grande evolução tecnologia e alta demanda de profissionais no mercado de trabalho."
+            ],
+            [
+                "gold",
+                "1º Lugar no Desafio empresarial - Café com Empresa - IF Barbacena",
+                "(08/2018 - Ibis Budget)",
+                "Tema: Desenvolver uma solução que faça com que o hotel Ibis seja referência em Barbacena, assim como em todas as cidades em que o hotéis da franquia são inaugurados.",
+                "Aproximar a população e o Ibis Budget através da implantação de hotspots fixos ou itinerantes em locais estratégicos com maior aglomeração de clientes em potencial."
+            ],
+            [
+                "silver",
+                "2º Lugar no Hackathon John Deere - Campus Party Brasil 10",
+                "(02/2017 - John Deere - Brasil)",
+                "Tema: Conectando com o campo (Tema livre)",
+                "Desenvolvemos o JDConnect solução que conecta toda a empresa através de um middleware e fornece informações através de um chat amigável para o cliente. Através desse chat o usuário poderá conversar diretamente com suas máquinas (colheitadeiras/tratores) , que em nossa solução se transformaram em bots."
+            ],
+            [
+                "gold",
+                "1º Lugar no Hackathon Prodemge - Governo de Minas Gerais - Campus Party MG",
+                "(11/2016 - Prodemge)",
+                "Tema: Obter formas inovadoras para melhorar o relacionamento entre o Estado, Cidadãos e Empresas, facilitando o acesso à informação, prestação de serviços, cidadania e aproximar o Estado do Cidadão. ",
+                "Desenvolvemos uma plataforma onde os usuários compartilham por áudio experiências sobre pontos turísticos de uma região. Com a localização do usuário sabíamos quais pontos sugerir e melhorar sua experiência ao visitar novas cidades."
+            ],
+            [
+                "gold",
+                "1º Lugar no Desafio empresarial - Café com Empresa - IF Barbacena",
+                "(07/2016 - Rivelli)",
+                "Tema: levar o processo de checklist automatizado para as fazendas",
+                "Adicionamos ao sistema atual de checklist da empresa um sistema de cache que possibilitara com que o motorista realize a checklist mesmo sem acesso a internet, transformando o sistema atual em um webapp."
+            ]
+        ],
         "text-qualifications": "Certificados e Diplomas",
         "qualifications": [
             ["Graduação em andamento em Sistemas Para Internet.",
             "(2015 - Data presente)",
-            "Instituto Federal do Sudeste de Minas Gerais, IF SUDESTE MG, Brasil."],
+            "Instituto Federal do Sudeste de Minas Gerais Campus Barbacena, IF SUDESTE MG, Brasil."],
             
             ["Técnico em Mecânica Industrial.",
             "(2010 - 2012)", 
             "SENAI - Departamento Regional de Minas Gerais, SENAI/DR/MG, Brasil."]
+        ],
+
+        "text-publications": "Publicações",
+        "publications": [
+            ["Middleware para controle Web de veículo semi-autônomo integrando comunicação Bluetooth e UDP com confiabilidade na Camada de Aplicação",
+            "(08/2017 - UFJF)",
+            "Foi desenvolvido um veículo semi-autônomo em Arduino que realiza comunicação Bluetooth com um Servidor através uma ponte de conexão(através de um protocolo UDP que implementa confiabilidade e um Middleware) e que se comunica com um cliente WebService em Microframework (Flask). O objetivo era obter o máximo de desempenho que um UDP pode oferecer sem ter perda de informações.",
+            "São João del Rei. Seminário sobre pesquisa e desenvolvimento de plataformas computacionais centradas na Web: uma visão da Engenharia de Software, 2017. v. 1. p. 57-58."]
         ],
         
         "text-contact": "Contato",
@@ -99,9 +165,11 @@ var language = {
         
         "principal-functions": ["FULL-STACK DEVELOPER"],
         "text-about": "About",
-        "info-about": "Currently finishing higher education in Barbacena - MG. I already develop some systems and applications\
-        based on languages ​​such as Ruby on Rails, Cordova, Java, among others, seeking to bring knowledge and\
-        learning in current market technologies. I already have a strong base in HTML, CSS and Javascript.",
+        "info-about": "I have contributed to my institution constantly with various projects and researches and also\
+         help the students inside the institution. I was monitor of complementary activities in the latest \
+         laboratory of robotics and technological innovation elaborating prototypes that use Arduino and Lego. \
+         I already develop applications, systems and services for several people, being autonomous and have ample \
+         knowledge in different Web technologies. Currently, I am learning Machine Learning in order to become a data scientist.",
         
         "text-info": "Info",
         "info":[
@@ -110,13 +178,13 @@ var language = {
             ["Email", "jpmdik@gmail.com"]
         ],
         
-        "text-skills": "Skills",
+        "text-skills": "Main Skills",
         "skills": [
-            ["HTML5", 90],
-            ["CSS", 75],
-            ["Jquery/JS", 75],
-            ["Bootstrap", 60],
-            ["Angular", 45]
+            ["Web", 90],
+            ["Python", 80],
+            ["Ruby on Rails", 80],
+            ["Machine Learning", 60],
+            ["Mobile", 60]
         ],
         
         "text-more": "More",
@@ -126,7 +194,7 @@ var language = {
             "Windows",
             "Ubuntu",
             "MySQL",
-            "Firebase"
+            "Firebase",
         ],
         
         "label-languages": "Languages",
@@ -162,6 +230,65 @@ var language = {
             "Monitoring of complementary activities of the subjects of the Technology Course\
              in Internet Systems in the Laboratory of Innovation and Robotics, using Arduino and Lego kits"],
         ],
+        "text-pAwards": "My Awards",
+        "awards": [
+            [
+                "gold",
+                "1st Place at Hackathon BNDES Business - Campus Party MG",
+                "(11/2018 - BNDES)",
+                "Topic: How to leverage BNDES business through social networks?",
+                "Our team developed Chatbot Bob (Bot of Banking) to bring BNDES tools interactively to the user, which, in addition to bringing convenience, brought ease to the user."
+            ],
+            [
+                "bronze",
+                "3rd Place at Hackathon BNDES Transparente - Campus Party MG",
+                "(11/2018 - BNDES)",
+                "Topic: Identify and respond fake news related to BNDES.",
+                "We created a system through the use of machine learning, which detected whether the news that was reported was Fake or not through a cross-data along with contextual analysis, in which weights were assigned, and the system gave us a verdict on the news. The system could also be learning over time, provided that this was done by those in charge of the system."
+            ],
+            [
+                "gold",
+                "1st place in the national artificial intelligence competition Brazilian Knowledge Discovery in Databases 2018, promoted by the 7th Brazilian Conference on Intelligent Systems (BRACIS).",
+                "(10/2018 - BRACIS)",
+                "Topic: Predicting palm oil production",
+                "Kaggle KDD BR 2018 machine learning competition champion solution: https://github.com/rafjaa/KDD-BR-2018/blob/master/solution.ipynb"
+            ],
+            [
+                "silver",
+                "2nd place in the I Startups Challenge of IFSudesteMG, Campus Juiz de Fora",
+                "(08/2018 - IFSudesteMG, campus Juiz de Fora)",
+                "Topic: Startups Challenge.",
+                "Our team presented an idea, named 'DevMirin', which would be a remote teaching platform that will influence young people to develop the logic of programming, which will be promising given the great evolution of technology and high demand of professionals in the labor market."
+            ],
+            [
+                "gold",
+                "1st Place in the Business Challenge - Café com Empresa - IF Barbacena",
+                "(08/2018 - Ibis Budget)",
+                "Topic: Develop a solution that will make the Ibis hotel a reference in Barbacena, as well as in all the cities in which the hotels of the franchise are inaugurated.",
+                "Bring the population and Ibis Budget closer by deploying fixed or roaming hotspots in strategic locations with the largest crowding of potential customers."
+            ],
+            [
+                "silver",
+                "2nd place at Hackathon John Deere - Campus Party Brasil 10",
+                "(02/2017 - John Deere - Brasil)",
+                "Topic: Connecting with the field (Free theme)",
+                "We develop the JDConnect solution that connects the entire enterprise through a middleware and provides information through a friendly chat to the client. Through this chat the user can talk directly with their machines (harvesters / tractors), which in our solution became bots."
+            ],
+            [
+                "gold",
+                "1st Place at Hackathon Prodemge - Government of Minas Gerais - Campus Party MG",
+                "(11/2016 - Prodemge)",
+                "Topic: Obtain innovative ways to improve the relationship between the State, Citizens and Companies, facilitating access to information, providing services, citizenship and bringing the Citizen State closer.",
+                "We have developed a platform where users share audio experiences about a region's sights. With the user's location we knew which points to suggest and improve their experience when visiting new cities."
+            ],
+            [
+                "gold",
+                "1st Place in the Business Challenge - Café com Empresa - IF Barbacena",
+                "(07/2016 - Rivelli)",
+                "Topic: Take the automated checklist process to farms",
+                "We added to the company's current checklist system a cache system that would allow the driver to perform the checklist even without access to the internet, transforming the current system into a webapp."
+            ]
+        ],
         "text-qualifications": "Training and Qualifications",
         "qualifications": [
             ["Graduation in progress in Internet Systems.",
@@ -171,6 +298,13 @@ var language = {
             ["Industrial Mechanical Technician.",
             "(2010 - 2012)", 
             "SENAI - Regional Department of Minas Gerais, SENAI/DR/MG, Brasil."]
+        ],
+        "text-publications": "Publications",
+        "publications": [
+            ["Middleware for semi-autonomous vehicle Web control integrating Bluetooth and UDP communication with Reliability in the Application Layer",
+            "(08/2017 - UFJF)",
+            "A semi-autonomous vehicle in Arduino has been developed that performs Bluetooth communication with a Server through a connection bridge (through a UDP protocol that implements reliability and a Middleware) and communicates with a WebService client in Microframework (Flask). The goal was to get the most performance a UDP can offer without losing information.",
+            "São João del Rei. Seminário sobre pesquisa e desenvolvimento de plataformas computacionais centradas na Web: uma visão da Engenharia de Software, 2017. v. 1. p. 57-58."]
         ],
         
         "text-contact": "Contact",
