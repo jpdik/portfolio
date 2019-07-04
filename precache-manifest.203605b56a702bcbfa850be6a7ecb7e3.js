67948921c62c5d8bbd48b3d22fd7cfd659676c6a
Joao Paulo de Melo <jpmdik@gmail.com>
2019-07-04 03:19:09 -0300

    Updates

---
+++ precache-manifest.203605b56a702bcbfa850be6a7ecb7e3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12c5d3b010feae93df8dd8fe2c1f7bb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "93bc8a04a090b40eaa52",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "35e38a68e122875df5a2",
+    "url": "/portfolio/static/css/main.adfcd111.chunk.css"
+  },
+  {
+    "revision": "93bc8a04a090b40eaa52",
+    "url": "/portfolio/static/js/2.884ecce5.chunk.js"
+  },
+  {
+    "revision": "35e38a68e122875df5a2",
+    "url": "/portfolio/static/js/main.a6113717.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4c73aaf8a479b61979ff57cdb0334756",
+    "url": "/portfolio/static/media/angularjs.4c73aaf8.png"
+  },
+  {
+    "revision": "0303924910053317f271eb33593409d4",
+    "url": "/portfolio/static/media/avatar.03039249.jpg"
+  },
+  {
+    "revision": "697f150ab8c7e2ff27144ea9edc2dc15",
+    "url": "/portfolio/static/media/bash.697f150a.png"
+  },
+  {
+    "revision": "2420183c454f00607988c5c591b682e7",
+    "url": "/portfolio/static/media/css.2420183c.png"
+  },
+  {
+    "revision": "adf68d54730e19c4db6ffc98fad28e2d",
+    "url": "/portfolio/static/media/firebase.adf68d54.png"
+  },
+  {
+    "revision": "9ba61b987344a498a69e0fd8b44603a0",
+    "url": "/portfolio/static/media/ionic.9ba61b98.png"
+  },
+  {
+    "revision": "3adbacb63c504c5e8fc0dee51015b87c",
+    "url": "/portfolio/static/media/logo.3adbacb6.png"
+  },
+  {
+    "revision": "7e7838e1fe3d04c23abd2dd1aa441e6e",
+    "url": "/portfolio/static/media/mongo.7e7838e1.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/portfolio/static/media/node.6dcf6999.png"
+  },
+  {
+    "revision": "c7422667d745ba2ce731f4765c15bb49",
+    "url": "/portfolio/static/media/poppins-bold-webfont.c7422667.woff"
+  },
+  {
+    "revision": "f42126601d14d9bd1eab8b63196d07cc",
+    "url": "/portfolio/static/media/poppins-medium-webfont.f4212660.woff"
+  },
+  {
+    "revision": "42a25c26425cbb63724bc85017b14e1b",
+    "url": "/portfolio/static/media/poppins-regular-webfont.42a25c26.woff"
+  },
+  {
+    "revision": "364d921a8c69f5373311b38ca6e976d8",
+    "url": "/portfolio/static/media/poppins-semibold-webfont.364d921a.woff"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "f18023dce4a1ce45b2f977f9b7598404",
+    "url": "/portfolio/static/media/redux.f18023dc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.9b3bc4dde94a3bc3daed47ffd56c78fd.js"
+  "/portfolio/precache-manifest.203605b56a702bcbfa850be6a7ecb7e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
