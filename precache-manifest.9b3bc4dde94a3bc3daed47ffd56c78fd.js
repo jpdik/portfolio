dbd0ddda4527c49aa1d5d43676c3b1ba8eba0aad
Joao Paulo de Melo <jpmdik@gmail.com>
2019-07-04 03:13:17 -0300

    Updates

---
+++ precache-manifest.9b3bc4dde94a3bc3daed47ffd56c78fd.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21b1f784943d842c1fef76c0da04eae",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "93bc8a04a090b40eaa52",
+    "url": "/react-gh-pages/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d87e8922592cbb8c371e",
+    "url": "/react-gh-pages/static/css/main.c1999fa3.chunk.css"
+  },
+  {
+    "revision": "93bc8a04a090b40eaa52",
+    "url": "/react-gh-pages/static/js/2.884ecce5.chunk.js"
+  },
+  {
+    "revision": "d87e8922592cbb8c371e",
+    "url": "/react-gh-pages/static/js/main.c503e736.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "4c73aaf8a479b61979ff57cdb0334756",
+    "url": "/react-gh-pages/static/media/angularjs.4c73aaf8.png"
+  },
+  {
+    "revision": "0303924910053317f271eb33593409d4",
+    "url": "/react-gh-pages/static/media/avatar.03039249.jpg"
+  },
+  {
+    "revision": "697f150ab8c7e2ff27144ea9edc2dc15",
+    "url": "/react-gh-pages/static/media/bash.697f150a.png"
+  },
+  {
+    "revision": "2420183c454f00607988c5c591b682e7",
+    "url": "/react-gh-pages/static/media/css.2420183c.png"
+  },
+  {
+    "revision": "adf68d54730e19c4db6ffc98fad28e2d",
+    "url": "/react-gh-pages/static/media/firebase.adf68d54.png"
+  },
+  {
+    "revision": "9ba61b987344a498a69e0fd8b44603a0",
+    "url": "/react-gh-pages/static/media/ionic.9ba61b98.png"
+  },
+  {
+    "revision": "3adbacb63c504c5e8fc0dee51015b87c",
+    "url": "/react-gh-pages/static/media/logo.3adbacb6.png"
+  },
+  {
+    "revision": "7e7838e1fe3d04c23abd2dd1aa441e6e",
+    "url": "/react-gh-pages/static/media/mongo.7e7838e1.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/react-gh-pages/static/media/node.6dcf6999.png"
+  },
+  {
+    "revision": "c7422667d745ba2ce731f4765c15bb49",
+    "url": "/react-gh-pages/static/media/poppins-bold-webfont.c7422667.woff"
+  },
+  {
+    "revision": "f42126601d14d9bd1eab8b63196d07cc",
+    "url": "/react-gh-pages/static/media/poppins-medium-webfont.f4212660.woff"
+  },
+  {
+    "revision": "42a25c26425cbb63724bc85017b14e1b",
+    "url": "/react-gh-pages/static/media/poppins-regular-webfont.42a25c26.woff"
+  },
+  {
+    "revision": "364d921a8c69f5373311b38ca6e976d8",
+    "url": "/react-gh-pages/static/media/poppins-semibold-webfont.364d921a.woff"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/react-gh-pages/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "f18023dce4a1ce45b2f977f9b7598404",
+    "url": "/react-gh-pages/static/media/redux.f18023dc.png"
+  }
+]);
